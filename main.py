cbea4ecb5c0f303ba698ef55281b1bca01b24de6
Zewa-plus <dmitry.bulavkin2016@yandex.ru>
2023-01-17 20:38:48 +0300

    Add new files

---
+++ main.py
@@ -0,0 +1,19 @@
+num1 = int(input('Введите перовое число: '))
+
+num2 = int(input('Введите второе число: '))
+
+num1 += 5
+
+print('Result:', num1 + num2)
+print('Result:', num1 - num2)
+print('Result:', num1 * num2)
+print('Result:', num1 / num2)
+print('Result:', num1 ** num2)
+print('Result:', num1 // num2)
+
+
+word = 'Hi'
+
+print(word * 2)
+
+word = False
